d14cef918e4e5ba15a7fcd9369b86c21d1a33d88
Thiago <thiagoisaiasbrg@gmail.com>
2018-05-08 17:56:06 -0300

    Configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      
+      mount_devise_token_auth_for 'User', at: 'auth'
+      
+      resources :users, only: %i[show] do
+        resources :accounts, except: %i[edit new], shallow: true
+      end
+    
+      resources :accounts, only: [] do
+        get 'current_balance', to: 'accounts#current_balance', on: :member
+        resources :transactions, except: %i[edit new], shallow: true
+      end
+    
+      resources :categories, only: %i[show index]
+      
+    end
+  end
 end
