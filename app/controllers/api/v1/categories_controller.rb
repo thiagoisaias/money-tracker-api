946c39c5e6bad905c8394c191165cb587dcd52b2
Thiago <thiagoisaiasbrg@gmail.com>
2018-05-21 18:18:23 -0300

    Turn authentication off

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CategoriesController < ApplicationController
-  before_action :authenticate_api_v1_user!
+  # before_action :authenticate_api_v1_user!
 
   def index
     @categories = Category.all.page params[:page]
@@ -15,5 +15,11 @@ class Api::V1::CategoriesController < ApplicationController
     @category = Category.find(params[:id])
     @category.destroy
     render json: {}, status: :no_content
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :initial_balance)
   end
 end
