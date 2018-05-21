b8349a7aa15eb4655b5b7fb251776f3ff1ba9cbe
Thiago <thiagoisaiasbrg@gmail.com>
2018-05-21 18:18:30 -0300

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,13 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth'
       
       resources :users, only: %i[show] do
-        resources :accounts, except: %i[edit new], shallow: true
+        resources :accounts, except: %i[edit new] do
+          get 'current_balance', to: 'accounts#current_balance', on: :member
+          resources :transactions, except: %i[edit new], on: :member
+        end
       end
     
-      resources :accounts, only: [] do
-        get 'current_balance', to: 'accounts#current_balance', on: :member
-        resources :transactions, except: %i[edit new], shallow: true
-      end
-    
-      resources :categories, only: %i[show index]
+      resources :categories, except: %i[edit new]
       
     end
   end
